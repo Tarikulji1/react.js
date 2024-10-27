c2b0a123430db999e083f88d9f4933167574d048
Tarikulji1 <islam.work.tarikul@gmail.com>
2024-10-27 22:57:37 +0530

    both process method

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,13 @@
 
-
+import Chai from './Chai'
 
 function App() {
   return (
+    <>
     <h1>Hello World! chai aur react</h1>
+    <Chai />
+    
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,14 @@
 
-
-
-
+import Chai from "./chai"
 
 function App() {
+
   return (
-    <h1>Chai aur React with vite | Tarikul</h1>
+    <>
+    <Chai />
+    <h1>Chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
