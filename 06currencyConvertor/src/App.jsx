b0291fee1bf54d376ca3665e274b1cc1213db6af
Tarikulji1 <islam.work.tarikul@gmail.com>
2024-10-29 23:16:45 +0530

    two side one image and one side converter app

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -27,9 +27,20 @@ function App() {
 
   return (
     <>
-    <div className='w-full' style={{backgroundImage: `url('https://images.pexels.com/photos/730568/pexels-photo-730568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`}}>
-      <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30' >
-        <h1 className='text-3xl bg-orange-500'>Currency Converter App</h1>
+    <div className='w-full h-screen bg-cover bg-center flex justify-center align-middle' style={{backgroundImage: `url('https://images.pexels.com/photos/730568/pexels-photo-730568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`}}>
+
+      <div className='bg-cover bg-center w-1/3 mr-0 mx-auto h-1/2 mt-60 rounded-tl-2xl rounded-bl-2xl' style={{backgroundImage: `url('https://images.pexels.com/photos/47344/dollar-currency-money-us-dollar-47344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`}}>
+      
+        <div className='bg-orange-500/5 border border-gray-60 p-3 backdrop-blur-sm rounded-tl-2xl flex flex-col justify-center text-center mt-40'>
+          <h1 className='text-3xl text-black bg-orange-200 font-bold rounded-lg p-1 mb-3'>Currency Converter App</h1>
+          <p className='text-xl text-black bg-orange-200 font-bold rounded-lg p-3'>Presenting new currency converter app is here. </p>
+        </div>
+      </div>
+
+      <div className='w-1/2 ml-0 h-1/2 mt-60 max-w-md mx-auto border border-gray-60 rounded-tr-2xl rounded-br-2xl p-5 backdrop-blur-sm bg-white/15' >
+
+        <h1 className='text-3xl text-center text-black font-bold bg-white/50 mx-auto border border-gray-60 rounded-lg p-3 m-5 mt-10 backdrop-blur-sm'>{from.toUpperCase()} to {to.toUpperCase()}</h1>
+
         <form 
           onSubmit={(e) => {
             e.preventDefault();
@@ -67,12 +78,14 @@ function App() {
                     amountDisable
                 />
             </div>
-            <button type='submit' className='w-full text-white px-4 py-3 rounded-lg'>
+            <button type='submit' className='w-full text-white px-4 py-3 rounded-lg bg-blue-700'>
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
         </form>
-      </div>
+      
 
+    
+      </div>
     </div>
     </>
   )
